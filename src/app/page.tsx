713bbbc7e5d22b422ac64ead27a6247608c03ecd
Hung Dang <106718735+HungMika@users.noreply.github.com>
2025-03-19 11:39:48 +0700

    add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return (
-    <main className="flex items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">Hello World</h1>
-    </main>
-  );
+  return <div></div>;
 }
